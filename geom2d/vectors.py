307f7b885f931ec1459508a31bd280a315751d3d
Kent <60073215+Shibaken0618@users.noreply.github.com>
2022-04-04 01:58:26 -0400

    updated

---
+++ geom2d/vectors.py
@@ -0,0 +1,8 @@
+from geom2d.point import Point
+from geom2d.vector import Vector
+
+def make_vector_between(p: Point, q:Point):
+    return q - p
+
+def make_versor(u:float, v:float):
+    return Vector(u,v).normalized()
