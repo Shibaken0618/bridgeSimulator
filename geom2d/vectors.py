e790ecafa2365d9c0738eed1e66f39d74c5c03e8
Kent <60073215+Shibaken0618@users.noreply.github.com>
2022-04-04 23:57:34 -0400

    updated

--- geom2d/vectors.py
+++ geom2d/vectors.py
@@ -6,3 +6,6 @@ def make_vector_between(p: Point, q:Point):
 
 def make_versor(u:float, v:float):
     return Vector(u,v).normalized()
+
+def make_versor_between(p:Point, q:Point):
+    return make_vector_between(p,q).normalized()
