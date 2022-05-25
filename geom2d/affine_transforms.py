a4426cec5425f1282b78667e7558c50cab3ba13e
Kent <60073215+Shibaken0618@users.noreply.github.com>
2022-05-25 17:59:32 -0400

    updated

--- geom2d/affine_transforms.py
+++ geom2d/affine_transforms.py
@@ -1,6 +1,7 @@
 import math
 from geom2d.affine_transf import AffineTransform
 from geom2d.point import Point
+from geom2d.interpolation import ease_in_out_t_sequence, interpolate
 
 def make_scale(sx:float, sy:float, center=Point(0,0)):
     return AffineTransform(
@@ -21,4 +22,18 @@ def make_rotation(radians:float, center = Point(0,0)):
         ty=center.y * one_minus_cos - center.x * sin,
         shx=-sin,
         shy=sin
+    )
+
+def ease_in_out_interpolation(start, end, steps):
+    t_seq = ease_in_out_t_sequence(steps)
+    return [__interpolated(start, end, t) for t in t_seq]
+
+def __interpolated(s: AffineTransform, e: AffineTransform, t):
+    return AffineTransform(
+        sx = interpolate(s.sx, e.sx, t),
+        sy = interpolate(s.sy, e.sy, t),
+        tx = interpolate(s.tx, e.tx, t),
+        ty = interpolate(s.ty, e.ty, t),
+        shx = interpolate(s.shx, e.shy, t),
+        shy = interpolate(s.shy, e.shy, t)
     )
