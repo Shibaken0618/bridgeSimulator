7cc6f1e26d5ab8d856f617ea238c3b46948b5821
Kent <60073215+Shibaken0618@users.noreply.github.com>
2022-05-27 15:12:07 -0400

    update

--- graphic/svg/primitives.py
+++ graphic/svg/primitives.py
@@ -1,10 +1,13 @@
-from geom2d import Segment, Rect, Circle
+from typing import List
+from geom2d import Point, Segment, Rect, Circle, Polygon
 from graphic.svg.attributes import attrs_to_str
 from graphic.svg.read import read_template
 
 __segment_template = read_template('line.txt')
 __rect_template = read_template('rect.txt')
 __circle_template = read_template('circle.txt')
+__polygon_template = read_template('polygon.txt')
+__polyline_template = read_template('polyline.txt')
 
 def segment(seg: Segment, attributes=()):
     return __segment_template \
@@ -27,5 +30,17 @@ def circle(circ: Circle, attributes=()):
         .replace('{{cx}}', str(circ.center.x)) \
         .replace('{{cy}}', str(circ.center.y)) \
         .replace('{{r}}', str(circ.radius)) \
-        .replace('{{attrs}}', attrs_to_str(attributes)) \
-    
+        .replace('{{attrs}}', attrs_to_str(attributes))
+
+def polygon(pol: Polygon, attributes=()):
+    return __polygon_template \
+        .replace('{{points}}', __format_points(pol.vertices)) \
+        .replace('{{attrs}}', attrs_to_str(attributes))
+
+def __format_points(points: List[Point]):
+    return ' '.join([f'{p.x},{p.y}' for p in points])
+
+def polyline(points: List[Point], attributes=()):
+    return __polyline_template \
+        .replace('{{points}}', __format_points(points)) \
+        .replace('{{attrs}}', attrs_to_str(attributes))
