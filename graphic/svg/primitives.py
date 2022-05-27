438ba82cd69b861b77a0841d9fbb0c45591a7566
Kent <60073215+Shibaken0618@users.noreply.github.com>
2022-05-26 20:46:15 -0400

    updated

--- graphic/svg/primitives.py
+++ graphic/svg/primitives.py
@@ -1,8 +1,9 @@
-from geom2d import Segment
+from geom2d import Segment, Rect
 from graphic.svg.attributes import attrs_to_str
 from graphic.svg.read import read_template
 
 __segment_template = read_template('line.txt')
+__rect_template = read_template('rect.txt')
 
 def segment(seg: Segment, attributes=()):
     return __segment_template \
@@ -10,4 +11,12 @@ def segment(seg: Segment, attributes=()):
         .replace('{{y1}}', str(seg.start.y)) \
         .replace('{{x2}}', str(seg.end.x)) \
         .replace('{{y2}}', str(seg.end.y)) \
+        .replace('{{attrs}}', attrs_to_str(attributes))
+
+def rectangle(rect: Rect, attributes=()):
+    return __rect_template \
+        .replace('{{x}}', str(rect.origin.x)) \
+        .replace('{{y}}', str(rect.origin.y)) \
+        .replace('{{width}}', str(rect.size.width)) \
+        .replace('{{height}}', str(rect.size.height)) \
         .replace('{{attrs}}', attrs_to_str(attributes))
