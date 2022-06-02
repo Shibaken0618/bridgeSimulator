9ff002ea9abfe9a19eab49b54c34626c30fe383f
Kent <60073215+Shibaken0618@users.noreply.github.com>
2022-06-02 19:47:33 -0400

    update

--- apps/circle_from_points/input.py
+++ apps/circle_from_points/input.py
@@ -1,7 +1,25 @@
+import re
+import json
+from geom2d import Point
+import pkg_resources as res
+
+
 def parse_points():
     return (
-        input(),
-        input(),
-        input(),
+        __point_from_string(input()),
+        __point_from_string(input()),
+        __point_from_string(input()),
     )
 
+
+def __point_from_string(string: str):
+    matches = re.match(r'(?P<x>\d+)\s(?P<y>\d+)', string)
+    return Point(
+        int(matches.group('x')),
+        int(matches.group('y'))
+    )
+
+
+def read_config():
+    config = res.resource_string(__name__, 'config.json')
+    return json.loads(config)
