cdbaa60a00e087381925c2a149f47cee2fbc9d32
Kent <60073215+Shibaken0618@users.noreply.github.com>
2022-06-09 18:45:49 -0400

    updated

--- apps/aff_transf_motion/main.py
+++ apps/aff_transf_motion/main.py
@@ -1,4 +1,10 @@
+from apps.aff_transf_motion.input import read_input
+from apps.aff_transf_motion.config import read_config
+from apps.aff_transf_motion.simulation import simulate
+
 if __name__ == '__main__':
-    print('Ready!')
+    (transform, primitives) = read_input()
+    config = read_config()
+    simulate(transform, primitives, config)
 
     
